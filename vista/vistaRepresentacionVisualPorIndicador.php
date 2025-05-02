b738c0459a9b0acd179105bf553e5ecbffc3a58b
Mechas <andrew2004.david2004@gmail.com>
2025-05-02 13:32:32 -0500

    Listar por nombre

--- vista/vistaRepresentacionVisualPorIndicador.php
+++ vista/vistaRepresentacionVisualPorIndicador.php
@@ -1,35 +1,20 @@
 <?php
 ob_start();
-?>
-<?php 
-	include '../controlador/configBd.php';
-	include '../controlador/ControlEntidad.php';
-	include '../controlador/ControlConexionPdo.php';
-	include '../modelo/Entidad.php';
-  	session_start();
-  	if($_SESSION['email']==null)header('Location: ../index.php');
+include '../controlador/configBd.php';
+include '../controlador/ControlEntidad.php';
+include '../controlador/ControlConexionPdo.php';
+include '../modelo/Entidad.php';
+session_start();
+if ($_SESSION['email'] == null) header('Location: ../index.php');
 
-	$permisoParaEntrar=false;
-	$listaRolesDelUsuario=$_SESSION['listaRolesDelUsuario'];
-	for($i=0;$i<count($listaRolesDelUsuario);$i++){
-		if($listaRolesDelUsuario[$i]->__get('nombre')=="admin" || $listaRolesDelUsuario[$i]->__get('nombre')=="Verificador")$permisoParaEntrar=true;
-	}
-	if(!$permisoParaEntrar)header('Location: ../vista/menu.php');
+$permisoParaEntrar = false;
+$listaRolesDelUsuario = $_SESSION['listaRolesDelUsuario'];
+for ($i = 0; $i < count($listaRolesDelUsuario); $i++) {
+    if ($listaRolesDelUsuario[$i]->__get('nombre') == "admin" || $listaRolesDelUsuario[$i]->__get('nombre') == "Verificador")
+        $permisoParaEntrar = true;
+}
+if (!$permisoParaEntrar) header('Location: ../vista/menu.php');
 
-?>
-<?php
-$arregloRepresentacionVisualPorIndicador=[];
-
-$objControlRepresentacionVisualPorIndicador = new ControlEntidad('represenvisualporindicador');
-$arregloRepresentacionVisualPorIndicador = $objControlRepresentacionVisualPorIndicador->listar();
-//var_dump($arregloRoles);
-
-//$boton = "";
-//if (isset($_POST['bt'])) $boton = $_POST['bt']; //En PHP 5.x
-
-//$boton = isset($_POST['bt']) ? $_POST['bt'] : ""; //En PHP 7
-
-$arreglo = [];
 $objControlRepresentacionVisualPorIndicador = new ControlEntidad('represenvisualporindicador');
 $arreglo = $objControlRepresentacionVisualPorIndicador->listar();
 
@@ -39,88 +24,93 @@ $fkidrepresenvisual = $_POST['txtRepresenVisual'] ?? '';
 
 switch ($boton) {
     case 'Guardar':
-        $datosRepresentacionVisualPorIndicador = ['fkidindicador' => $fkidindicador, 'fkidrepresenvisual' => $fkidrepresenvisual];
-        $objRepresentacionVisualPorIndicador = new Entidad($datosRepresentacionVisualPorIndicador);
-        $objControlRepresentacionVisualPorIndicador = new ControlEntidad('represenvisualporindicador');
-        $objControlRepresentacionVisualPorIndicador->guardar($objRepresentacionVisualPorIndicador);
+        $datos = ['fkidindicador' => $fkidindicador, 'fkidrepresenvisual' => $fkidrepresenvisual];
+        $obj = new Entidad($datos);
+        $objControlRepresentacionVisualPorIndicador->guardar($obj);
         header('Location: vistaRepresentacionVisualPorIndicador.php');
         break;
     case 'Borrar':
-        $datosRepresentacionVisualPorIndicador = ['fkidindicador' => $fkidindicador, 'fkidrepresenvisual' => $fkidrepresenvisual];
-        $objRepresentacionVisualPorIndicador = new Entidad($datosRepresentacionVisualPorIndicador);
+        $datos = ['fkidindicador' => $fkidindicador, 'fkidrepresenvisual' => $fkidrepresenvisual];
+        $obj = new Entidad($datos);
         $objControlRepresentacionVisualPorIndicador->borrar('fkidindicador', $fkidindicador, 'fkidrepresenvisual', $fkidrepresenvisual);
         header('Location: vistaRepresentacionVisualPorIndicador.php');
         break;
 }
 
+
+$arregloIndicadoresConsulta = [];
+$objcontrolIndicador = new ControlEntidad('indicador');
+$arregloIndicadores = $objcontrolIndicador->listar();
+foreach ($arregloIndicadores as $ind) {
+    $arregloIndicadores[$ind->__get('id')] = $ind->__get('nombre');
+}
+
+$arregloRepresenVisualConsulta = [];
+$objcontrolRepresenVisual = new ControlEntidad('represenvisual');
+$arregloRepresenVisuales = $objcontrolRepresenVisual->listar();
+foreach ($arregloRepresenVisuales as $vis) {
+    $arregloRepresenVisuales[$vis->__get('id')] = $vis->__get('nombre');
+}
 ?>
+
 <?php include "../vista/base_ini_head.html" ?>
 <?php include "../vista/base_ini_body.html" ?>
+
 <div class="container-xl">
-	<div class="table-responsive">
-		<div class="table-wrapper">
-			<div class="table-title">
-				<div class="row">
-					<div class="col-sm-6">
-						<h2 class="miEstilo">Gestión <b>RepresentacionVisualPorIndicador</b></h2>
-					</div>
-					<div class="col-sm-6">
-						<a href="#crudModal" class="btn btn-primary" data-toggle="modal"><i class="material-icons">&#xE84E;</i> <span>Gestión F</span></a>
-					</div>
-				</div>
-			</div>
-			<table class="table table-striped table-hover">
-				<thead>
-					<tr>
-						<th>
-							<span class="custom-checkbox">
-								<input type="checkbox" id="selectAll">
-								<label for="selectAll"></label>
-							</span>
-						</th>
-						<th>Indicador</th>
-						<th>RepresenVisual</th>
-						<th>Actions</th>
-					</tr>
-				</thead>
-				<tbody>
-					<?php
-					for($i = 0; $i < count($arreglo); $i++){
-					?>
-						<tr>
-							<td>
-								<span class="custom-checkbox">
-									<input type="checkbox" id="checkbox1" name="options[]" value="1">
-									<label for="checkbox1"></label>
-								</span>
-							</td>
-							<td><?php echo $arreglo[$i]->__get('fkidindicador');?></td>
-							<td><?php echo $arreglo[$i]->__get('fkidrepresenvisual');?></td>
-							<td>
-								<a href="#editar" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip">&#xE254;</i></a>
-								<a href="#borrar" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip">&#xE872;</i></a>
-							</td>
-						</tr>
-					<?php
-					}
-					?>
-				</tbody>
-			</table>
-			<div class="clearfix">
-				<div class="hint-text">Showing <b>5</b> out of <b>25</b> entries</div>
-				<ul class="pagination">
-					<li class="page-item disabled"><a href="#">Previous</a></li>
-					<li class="page-item"><a href="#" class="page-link">1</a></li>
-					<li class="page-item"><a href="#" class="page-link">2</a></li>
-					<li class="page-item active"><a href="#" class="page-link">3</a></li>
-					<li class="page-item"><a href="#" class="page-link">4</a></li>
-					<li class="page-item"><a href="#" class="page-link">5</a></li>
-					<li class="page-item"><a href="#" class="page-link">Next</a></li>
-				</ul>
-			</div>
-		</div>
-	</div>        
+    <div class="table-responsive">
+        <div class="table-wrapper">
+            <div class="table-title">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <h2 class="miEstilo">Gestión <b>RepresentacionVisualPorIndicador</b></h2>
+                    </div>
+                    <div class="col-sm-6">
+                        <a href="#crudModal" class="btn btn-primary" data-toggle="modal">
+                            <i class="material-icons">&#xE84E;</i> <span>Gestión F</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>
+                            <span class="custom-checkbox">
+                                <input type="checkbox" id="selectAll">
+                                <label for="selectAll"></label>
+                            </span>
+                        </th>
+                        <th>Indicador</th>
+                        <th>Representación Visual</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($arreglo as $item): ?>
+                        <tr>
+                            <td>
+                                <span class="custom-checkbox">
+                                    <input type="checkbox" name="options[]" value="1">
+                                    <label></label>
+                                </span>
+                            </td>
+                            <td><?= $arregloIndicadores[$item->__get('fkidindicador')] ?? 'Desconocido' ?></td>
+                            <td><?= $arregloRepresenVisuales[$item->__get('fkidrepresenvisual')] ?? 'Desconocido' ?></td>
+                            <td>
+                                <a href="#editar" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip">&#xE254;</i></a>
+                                <a href="#borrar" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip">&#xE872;</i></a>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>        
 </div>
+
+<?php include "../vista/basePie.html" ?>
+<?php ob_end_flush(); ?>
+
 
 <div id="crudModal" class="modal fade">
 	<div class="modal-dialog">
